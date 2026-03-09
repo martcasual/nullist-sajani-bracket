01adb3731ee0ed13a70859f808418de012cea060
martcasual <joesaarmart@gmail.com>
2026-03-09 20:13:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import Leaderboard from "./components/Leaderboard"
 export default function App(){
 
 const [teams] = useState([
-{name:"Team 1"},
-{name:"Team 2"},
-{name:"Team 3"},
-{name:"Team 4"},
-{name:"Team 5"},
-{name:"Team 6"},
-{name:"Team 7"},
-{name:"Team 8"},
-{name:"Team 9"},
-{name:"Team 10"},
-{name:"Team 11"},
-{name:"Team 12"},
-{name:"Team 13"},
-{name:"Team 14"},
-{name:"Team 15"},
-{name:"Team 16"},
+{name:"Arsenal"},
+{name:"Barcelona"},
+{name:"Real Madrid"},
+{name:"Bayern"},
+{name:"Liverpool"},
+{name:"PSG"},
+{name:"Juventus"},
+{name:"Dortmund"},
+{name:"Chelsea"},
+{name:"Inter"},
+{name:"AC Milan"},
+{name:"Ajax"},
+{name:"Porto"},
+{name:"Benfica"},
+{name:"Napoli"},
+{name:"Atletico"}
 ])
 
 const [pred,setPred]=useState({})
